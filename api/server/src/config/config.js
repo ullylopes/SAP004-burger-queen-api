2a8fb26daa86c923651b0ffcd355e4c0ed911c98
Ully Lopes <ullylopes@ymail.com>
2020-08-28 19:07:48 -0300

    Criando banco de dados

---
+++ api/server/src/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": example,
+    "database": "new_store_development",
+    "host": "db",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "postgres",
+    "password": example,
+    "database": "new_store_test",
+    "host": "db",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "postgres",
+    "password": example,
+    "database": "new_store_production",
+    "host": "db",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
